52c3029c5b6edd890866c11935399bf9c375b967
Eric Yi <erickanginyi@gmail.com>
2021-08-31 09:17:59 -0400

    commit tree 67a2010f134a3446a77d67be71fee924d1ace249

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+const saturdayFun = (action = "roller-skate") => {
+    return `This Saturday, I want to ${action}!`;
+}
+
+const mondayWork = (action = "go to the office") => {
+    return `This Monday, I will ${action}.`
+}
+
+const wrapAdjective = (string = "*") => {
+    return function (adjective = "special") {
+        return `You are ${string}${adjective}${string}!`;
+    }   
+}
